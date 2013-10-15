6a09f65ecd0be1ad344637175eeec67f0efdb11e
p-andreades <p.andreades@gmail.com>
2013-10-15 14:54:28 +0100

    Initiated Files For Simple Optical (SO) Network

---
+++ SO/SO_Network.sv
@@ -0,0 +1,6 @@
+// IP Block    : SO
+// Function    : Network
+// Module name : SO_Network
+// Description :
+// Uses        : config.sv, SO_AllocSimple.sv, SO_TxSimple.sv and SO_PhotonicSwitch.sv
+// Notes       :
