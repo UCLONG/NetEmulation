54a7899406ca582e4e28f87f1cc24bd049b8805d
Danny Nicholls <danny.nicholls.09@ucl.ac.uk>
2014-02-23 15:03:03 +0000

    Testing removal of data from branch
    This branch is going to have only the code from v1_2

--- SO/SO_Network.sv
+++
@@ -1,6 +0,0 @@
-// IP Block    : SO
-// Function    : Network
-// Module name : SO_Network
-// Description :
-// Uses        : config.sv, SO_AllocSimple.sv, SO_TxSimple.sv and SO_PhotonicSwitch.sv
-// Notes       :
